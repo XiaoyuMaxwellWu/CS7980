1155965d8c7f2012b929e2b108a1e65d75ab7748
maxwellxiaoyuwu <xiaoyu.wu@northeastern.edu>
2022-10-23 15:51:32 -0700

    improve distributed lock strategy

--- src/main/java/com/eventualconsistency/demo/controller/DistributedLockController.java
+++ src/main/java/com/eventualconsistency/demo/controller/DistributedLockController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import org.omg.PortableServer.THREAD_POLICY_ID;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
@@ -48,7 +49,7 @@ public class DistributedLockController {
   // (1) 3. no entry, no lock, add a new redis lock, with entry key as key, unique id as value, and a time(2s)
   // 4. fetch entry from mysql, check if the lock with entry key has same unique id, has, update
   // 5. not the same one, not update
-  // (2) 6. no entry, has lock, sleep 100 ms, return to step 3
+  // (2) 6. no entry, has lock, sleep 100 ms multiple times till redis has the entry 
   // 7. mysql updates, and then redis lock deletes, and then updates redis
   @PostMapping("/findByKeyLock")
   public ResponseEntry findByKeyLock(@RequestBody Map<String, Object> requestInfo)
@@ -63,33 +64,28 @@ public class DistributedLockController {
       return new ResponseEntry(key, value, true);
     }
     String uuid = UUID.randomUUID().toString();
-    // keep waiting until lock is expired
-    while (!redisTemplate.opsForValue().setIfAbsent(key, uuid, 2, TimeUnit.SECONDS)) {
+    // no lock, add a lock 
+    if (redisTemplate.opsForValue().setIfAbsent(key, uuid, 2, TimeUnit.SECONDS)) {
+      log.info("look from Mysql");
+      MysqlTab mysqlTab = mysqlRepository.findByCsKey(key);
+      if (mysqlTab == null) {
+        return null;
+      }
+      Object tryLock = redisTemplate.opsForValue().get(key);
+      // update redis only if the key has lock with the same request ID 
+      if (tryLock != null && uuid.equals(tryLock.toString())) {
+        hashOperations.put(Constant.KEY, key, mysqlTab.getCsValue());
+      }
+      return new ResponseEntry(key, mysqlTab.getCsValue(), false);
+    }
+    // has lock, keep waiting until there is a value in redis
+    while ((tryEntry = hashOperations.get(Constant.KEY, key)) == null) {
       Thread.sleep(100);
     }
-    log.info("look from Mysql");
-    MysqlTab mysqlTab = mysqlRepository.findByCsKey(key);
-    if (mysqlTab == null) {
-      return null;
-    }
-    Object tryLock = redisTemplate.opsForValue().get(key);
-    // update redis only if the key has lock with the same request ID 
-    if (tryLock != null && uuid.equals(tryLock.toString())) {
-      hashOperations.put(Constant.KEY, key, mysqlTab.getCsValue());
-    }
-    return new ResponseEntry(key, mysqlTab.getCsValue(), false);
+    return new ResponseEntry(key, tryEntry.toString(), true);
 
   }
 
-  @PostMapping("/test")
-  @GetMapping("/test")
-  public void test() throws InterruptedException {
-//    redisTemplate.opsForValue().set("K2", "aaa", 2, TimeUnit.SECONDS);
-    System.out.println(redisTemplate.opsForValue().setIfAbsent("K2", "b", 2, TimeUnit.SECONDS));
-    System.out.println(redisTemplate.opsForValue().get("K2"));
-  }
-  
-  
   @PostMapping("/updateMysqlLock")
   public void updateMysqlLock(@RequestBody MysqlTab mysqlTab) {
     // update mysql with new entry
